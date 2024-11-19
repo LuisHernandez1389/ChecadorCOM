1132327409f042adf406ec4b066e5698be0bcf2e
LuisHernandez1389 <luilmhg70@gmail.com>
2024-11-19 16:25:52 -0700

    dropzone

--- fileHandler.js
+++ fileHandler.js
@@ -40,3 +40,46 @@ window.addEventListener('load', function() {
         renderAccordion();
     }
 });
+
+const dropzone = document.getElementById('dropzone');
+const fileInput = document.getElementById('input-excel');
+const fileList = document.getElementById('fileList');
+
+dropzone.addEventListener('click', () => {
+    fileInput.click();
+});
+
+dropzone.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dropzone.classList.add('dragover');
+});
+
+dropzone.addEventListener('dragleave', () => {
+    dropzone.classList.remove('dragover');
+});
+
+dropzone.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dropzone.classList.remove('dragover');
+    const files = e.dataTransfer.files;
+    handleFiles(files);
+});
+
+// Modificación aquí: Maneja los archivos arrastrados
+fileInput.addEventListener('change', (e) => {
+    const files = e.target.files;
+    handleFiles(files);
+});
+
+function handleFiles(files) {
+    fileList.innerHTML = '';
+    for (const file of files) {
+        const listItem = document.createElement('div');
+        listItem.className = 'alert alert-secondary';
+        listItem.textContent = file.name;
+        fileList.appendChild(listItem);
+
+        // Llama a handleFile para procesar el archivo
+        handleFile({ target: { files: [file] } });
+    }
+}
